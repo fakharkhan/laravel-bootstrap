fa7dab41200c569608479b02c051aaf0ee6bb1e3
Fakhar Khan <fakhar@softpyramid.com>
2014-10-26 12:01:03 +0500

    navbar-brand added

--- app/views/shared/guest/navbar.blade.php
+++ app/views/shared/guest/navbar.blade.php
@@ -1,5 +1,6 @@
 <!--Top Navigation Bar-->
 <ul class="nav navbar-nav">
+    <a href="/" class="navbar-brand">[YOUR-APP]</a>
     <li class="active"><a href="/">Home</a></li>
     <li><a href="#about">About</a></li>
     <li><a href="#contact">Contact</a></li>
